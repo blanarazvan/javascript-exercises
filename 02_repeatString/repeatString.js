94934ce3abdadea8f5fa80f2ab15e6c4d934c2e0
blanarazvan <razvanblana331@gmail.com>
2025-03-18 02:04:19 +0200

    Comment inside 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let i = 1;
+    let newString = '';
+    if(number<0){
+        return 'ERROR';
+    }
+    
+    while(i<=number){
+        newString += string;
+        i++;
+        }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(fullArray, ...eliminatedNumbers) {
+    let comparedNumber = 0;
+    let newArray = [];
+    let k = 0, j = 0;
+    for ( let i = 0; i< fullArray.length; i++){// Reading full Array
+        comparedNumber = fullArray[i];
+        BlackList = []; 
+        for ( j = 0; j < eliminatedNumbers.length; j++){// Acting like an Array
+            // Compare comparedNumber with each value from eliminatedNumbers
+            // before adding into newArray
+        if (comparedNumber != eliminatedNumbers[j]){
+            
+            if (previousNumber != comparedNumber){
+            newArray[k] = comparedNumber;
+            previousNumber = comparedNumber;
+            k++;
+            } 
+        } 
+    }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
