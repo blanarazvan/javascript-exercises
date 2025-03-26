93f61d36621a0e969d2e6d6fb85e2d4e5456405b
blanarazvan <razvanblana331@gmail.com>
2025-03-26 23:45:12 +0200

    Almost done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
 
+  // Create a new reversed string for comparison
+  const reversedString = cleanedString.split('').reverse().join('');
+
+  // Return the outcome of the comparison which will either be true or false
+  return cleanedString === reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// racecar = .length = 7/2 = 3.5 = 3 
+// .length = 10/2 = 5
+// 0  6-0+1     0123456
+// 0 6-0 1
+
+// 9/2 = 4.5 = 4 
