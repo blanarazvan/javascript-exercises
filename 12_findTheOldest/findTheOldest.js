1785ff870370218db85f458a980a81dae5380dc9
blanarazvan <razvanblana331@gmail.com>
2025-03-27 15:54:20 +0200

    All exercises done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+//Find the oldest person 
+// First find out their age by yearOfDeath - yearOfBirth
+// Compare all the elements and get the greater one
+// Return the name of that person 
+    const oldestPerson = people.reduce((oldest, person) => {
+        let age;
+        if(person.yearOfDeath === undefined){
+            const date = new Date().getFullYear();
+            age = date - person.yearOfBirth;
+        } else {age = person.yearOfDeath-person.yearOfBirth;}
+        if(age > oldest.age){
+            return {name: person.name, age: age};
+        }
+        return oldest;
+    }, {name: '', age: 0});
+
+    return oldestPerson;
 
 };
 
